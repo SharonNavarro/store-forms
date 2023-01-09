a391348715b008def70d36ba54db69bd8fb2c279
SharonNavarro <shanmowrk@gmail.com>
2023-01-09 16:27:06 -0500

    learn formControl's state, dynamic and reactive

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicFormComponent implements OnInit {
 
+  nameField = new FormControl('soy un controlador');
+
   constructor() { }
 
   ngOnInit(): void {
+    this.nameField.valueChanges.subscribe(console.log)
+  }
+
+  getNameValue() {
+    console.log(this.nameField.value);
   }
 
 }

40fb9975b58147ec0e6bc0901367a5a20327f184
SharonNavarro <shanmowrk@gmail.com>
2023-01-10 15:35:36 -0500

    ReactiveForms with FormBuilder

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
@@ -7,31 +7,19 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    color: new FormControl('#000000'),
-    date: new FormControl(''),
-    month: new FormControl(''),
-    age: new FormControl(12),
-    password: new FormControl(''),
-    price: new FormControl('50'),
-    week: new FormControl(''),
-    time: new FormControl(''),
-    search: new FormControl(''),
-    description: new FormControl(''),
-    category: new FormControl('category-2'),
-    tag: new FormControl(),
-    agree: new FormControl(false),
-    gender: new FormControl(''),
-    zone: new FormControl('')
-  })
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.buildForm();
+   }
 
   ngOnInit(): void {
-    this.nameField.valueChanges.subscribe(console.log)
+    this.nameField.valueChanges.subscribe(console.log);
+    this.form.valueChanges.subscribe(value => {
+      console.log(value)
+    })
   }
 
   getNameValue() {
@@ -122,8 +110,35 @@ export class BasicFormComponent implements OnInit {
     return this.form.get('description');
   }
 
-  save() {
-    console.log(this.form.value)
+  save(event) {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      this.form.markAllAsTouched;
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(10)]],
+      email: [''],
+      phone: ['', Validators.required],
+      color: ['#000000'],
+      date: [''],
+      month:[''],
+      age: [12],
+      password: [''],
+      price: ['50'],
+      week: [''],
+      time: [''],
+      search: [''],
+      description: [''],
+      category: ['category-2'],
+      tag: [],
+      agree: [false],
+      gender: [''],
+      zone: ['']
+    });
   }
 
 }

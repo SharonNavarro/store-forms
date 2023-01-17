c54b1590e5b2e37384af440f7f2ceb0f771b7242
SharonNavarro <shanmowrk@gmail.com>
2023-01-17 18:51:31 -0500

    create my own library with Control Value Accesor

--- src/app/admin/products/products.module.ts
+++ src/app/admin/products/products.module.ts
@@ -8,7 +8,7 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductCreateComponent } from './components/product-create/product-create.component';
 import { ProductEditComponent } from './components/product-edit/product-edit.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -19,6 +19,7 @@ import { ProductEditComponent } from './components/product-edit/product-edit.com
   ],
   imports: [
     CommonModule,
+    SharedModule,
     ReactiveFormsModule,
     MaterialModule,
     ProductsRoutingModule
